162714b0d94397d1574db07129085ce9299c5da1
jowii26 <120990158+jowii26@users.noreply.github.com>
2024-05-08 14:04:40 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCATZzSTWWeIvtklo9-InlhbRFSHT2ekzs",
+    authDomain: "quantombot-28b47.firebaseapp.com",
+    projectId: "quantombot-28b47",
+    storageBucket: "quantombot-28b47.appspot.com",
+    messagingSenderId: "1008159619446",
+    appId: "1:1008159619446:web:9b9a9566f27176872ba257",
+    measurementId: "G-WKEP3HYG8V"
+  };
